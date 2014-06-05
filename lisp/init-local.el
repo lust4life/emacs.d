bb365f6bd52bed78f286688ebc3b82e6106be47c
$+J <lust4life.jun@gmail.com>
2014-06-05 18:30:43 +0800

    add cperl mode, add customer init

---
+++ lisp/init-local.el
@@ -0,0 +1,21 @@
+;; author: $+J
+;; e-mail: lust4life.jun@gmail.com
+
+
+;; 半透明
+(when window-system
+  (setq alpha-list '((80 50) (100 100)))
+  (defun wf-toggle-alpha ()
+    (interactive)
+    (let ((h (car alpha-list)))
+      ((lambda (a ab)
+         (set-frame-parameter (selected-frame) 'alpha (list a ab))
+         (add-to-list 'default-frame-alist (cons 'alpha (list a ab)))
+         ) (car h) (car (cdr h)))
+      (setq alpha-list (cdr (append alpha-list (list h))))))
+  (wf-toggle-alpha))
+
+;; 替换默认的 cperl mode
+(require 'prelude-perl)
+
+(provide 'init-local)
