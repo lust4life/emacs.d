2b1aba4222c99971da02c22ea4b289c7e71d6eee
$+j <lust4life.jun@gmail.com>
2015-03-28 23:31:56 +0800

    add erc settings

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -45,4 +45,32 @@
 (require 'o-blog)
 
 
+;; about erc
+
+;; joining && auto joinning
+(erc-autojoin-mode t)
+(setq erc-autojoin-channels-alist
+      '((".*\\.freenode.net" "#emacs" "#perl" "#perl6" "##csharp" "##asp.net")))
+
+;; check channels
+(erc-track-mode t)
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "324" "329" "332" "333" "353" "477"))
+
+;; don't show any of this
+(setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+
+(defun djcb-erc-start-or-switch ()
+  "Connect to ERC, or switch to last active buffer"
+  (interactive)
+  (if (get-buffer "irc.freenode.net:6667") ;; ERC already active?
+      (erc-track-switch-buffer 1) ;; yes: switch to last active
+    (when (y-or-n-p "Start ERC? ") ;; no: maybe start ERC
+      (erc :server "irc.freenode.net" :port 6667 :nick "lustlife" :full-name "lustlife.Jun" :password "lust4life"))))
+
+;; switch to ERC with Ctrl+c e
+(global-set-key (kbd "C-c e") 'djcb-erc-start-or-switch) ;; ERC
+
+
+
+
 (provide 'init-local)
