569bed4e7415668347718c81885611b5f0d113ba
$+j <lust4life.jun@gmail.com>
2015-11-09 09:57:36 +0800

    add reveal js root

--- custom.el
+++ custom.el
@@ -30,6 +30,7 @@
    (quote
     (("zh" "  <p>Created : <span name=\"create-date\">%d</span></p>
   <p>Modified: <span name=\"modified-date\">%C</span></p>"))))
+ '(org-reveal-root "http://libs.cdnjs.net/reveal.js/3.1.0/")
  '(org-startup-truncated t)
  '(safe-local-variable-values
    (quote
