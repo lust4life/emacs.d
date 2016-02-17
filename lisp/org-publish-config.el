bf6fdcccb789862da8b847d6537f6af311503aec
$+j <lust4life.jun@gmail.com>
2016-02-17 17:10:25 +0800

    fix regexp

--- lisp/org-publish-config.el
+++ lisp/org-publish-config.el
@@ -13,7 +13,7 @@
                                    :recursive t
                                    :publishing-function org-html-publish-to-html
                                    :auto-sitemap nil ; 这里先尝试不用生成 sitemap，貌似比较费时
-                                   :exclude ".*20\(08\|09\|10\|11\|12\|13\|14\|15\).*"
+                                   :exclude ".*20\\(08\\|09\\|10\\|11\\|12\\|13\\|14\\|15\\).*"
                                    :section-numbers nil
                                         ;:sitemap-sort-files anti-chronologically ;; 这里会导致程序无响应
                                    )
@@ -27,7 +27,7 @@
 (defun publish-lust4life-site nil
   "Publish lust4life site project."
   (interactive)
-  (org-publish-project "lust4life" nil t)
+  (org-publish-project "lust4life")
   )
 
 (provide 'org-publish-config)
