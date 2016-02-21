278fe0a766d6b41396298b08823be2c35cadadcd
$+j <lust4life.jun@gmail.com>
2016-02-21 14:08:26 +0800

    remove exclude cause no too many file error

--- lisp/org-publish-config.el
+++ lisp/org-publish-config.el
@@ -13,7 +13,7 @@
                                    :recursive t
                                    :publishing-function org-html-publish-to-html
                                    :auto-sitemap nil ; 这里先尝试不用生成 sitemap，貌似比较费时
-                                   :exclude ".*20\\(08\\|09\\|10\\|11\\|12\\|13\\|14\\|15\\).*"
+                                   ;;:exclude ".*20\\(08\\|09\\|10\\|11\\|12\\|13\\|14\\|15\\).*"
                                    :section-numbers nil
                                         ;:sitemap-sort-files anti-chronologically ;; 这里会导致程序无响应
                                    )
