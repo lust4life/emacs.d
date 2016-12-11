851c9596d41a370a02b30ec4bdb6fa07aa2c6a65
$+j <lust4life.jun@gmail.com>
2016-12-11 16:35:57 +0800

    update package selected packages

--- custom.el
+++ custom.el
@@ -3,12 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes
    (quote
     ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
- '(session-use-package t nil (session))
-
  '(org-agenda-files nil)
  '(org-export-async-init-file
    "c:/Users/lust4/AppData/Roaming/.emacs.d/init-4-org-export.el")
@@ -29,9 +27,12 @@
     <p>Modified: <span name=\"modified-date\">%C</span></p>"))))
  '(org-reveal-root "http://lust4life.github.io/slide/reveal/")
  '(org-startup-truncated t)
+ '(package-selected-packages
+   (quote
+    (yari yaml-mode yagist whole-line-or-region whitespace-cleanup-mode wgrep vc-darcs unfill undo-tree tidy textile-mode tagedit switch-window sql-indent smex smarty-mode slime-company skewer-less session scss-mode scratch sass-mode ruby-hash-syntax rspec-mode robe rinari regex-tool redshank rainbow-mode rainbow-delimiters projectile-rails project-local-variables pip-requirements php-mode paredit-everywhere page-break-lines ox-reveal origami org-pomodoro org-fstree org-cliplink nlinum mwe-log-commands multiple-cursors move-dup mmm-mode markdown-mode magit-gh-pulls lua-mode lively less-css-mode ledger-mode json-mode js-comint ipretty intero indent-guide immortal-scratch ibuffer-vc htmlize hl-sexp hippie-expand-slime hindent highlight-symbol highlight-quoted highlight-escape-sequences hayoo guide-key goto-gem gnuplot gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed fullframe fsharp-mode flycheck-package flycheck-ledger flycheck-elm flycheck-clojure fill-column-indicator expand-region exec-path-from-shell erlang elm-mode elisp-slime-nav elein dsvn disable-mouse dirtree dired-sort dired+ diminish diff-hl default-text-scale darcsum csv-nav csv-mode css-eldoc crontab-mode counsel company-anaconda color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized coffee-mode cljsbuild-mode cl-lib-highlight cask-mode bundler bug-reference-github browse-kill-ring browse-at-remote avy auto-compile anzu aggressive-indent)))
+ '(session-use-package t nil (session))
  '(user-full-name "$+j")
- '(user-mail-address "lust4life.jun@gmail.com")
- )
+ '(user-mail-address "lust4life.jun@gmail.com"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
