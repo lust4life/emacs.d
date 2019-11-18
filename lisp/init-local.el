7b308983fcdaa868521821bc1e4ccad7b3053382
Jun <lust4life.jun@gmail.com>
2019-11-18 20:01:43 +0800

    using powershell

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -7,7 +7,8 @@
 ;; exports your Org documents to reveal.js presentations
 ;; (require-package 'org-re-reveal)
 ;; (require 'org-re-reveal)
-
+(require-package 'ox-reveal)
+(require 'ox-reveal)
 
 ;; directory tree
 (require-package 'dirtree)
@@ -108,8 +109,29 @@
   (insert (concat "[[" imgPath "]]"))
   )
 
-(global-set-key (kbd "C-c v") 'paste-img)
+(defun my-org-screenshot ()
+  "Take a screenshot into a time stamped unique-named file in the
+same directory as the org-buffer and insert a link to this file."
+  (interactive)
+  (setq imgName (read-string "fileName:"))
+  (setq imgName (concat imgName ".png"))
+  ;; (setq filename
+  ;;       (concat
+  ;;        (make-temp-name
+  ;;         (concat (buffer-file-name)
+  ;;                 "_"
+  ;;                 (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+  (setq filename (concat "./img/" (replace-regexp-in-string "\.[^.]*$" "" (buffer-name)) "/" imgName))
+  (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+  (insert (concat "[[file:" filename "]]"))
+  (org-display-inline-images))
+
+(global-set-key (kbd "C-c v") 'my-org-screenshot)
 
 (setq system-time-locale "C")
+
+
+
+
 
 (provide 'init-local)
